0a42ae57b0f9b55c874e06170c902ccf98edca09
CEDMD\crme008 <marina.burlacu@iongroup.com>
2023-03-31 18:13:18 +0300

    add functions

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+
 import { AppComponent } from './app.component';
 import { TimerComponent } from './components/timer/timer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { TimerComponent } from './components/timer/timer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- project/src/app/components/timer/timer.component.ts
+++ project/src/app/components/timer/timer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Observable, Subject, takeUntil, timer } from 'rxjs';
 
 @Component({
   selector: 'app-timer',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./timer.component.css']
 })
 export class TimerComponent {
+  minutes:number = 0
+  seconds:number = 0
+  milliseconds:number = 0
+
+  stop = new Subject()
+
+  timer:Observable<any> | undefined
+
+  ngOnInit() {
+    this.timer = timer(100,100)  
+    .pipe(takeUntil(this.stop))
+    }
+ 
+    
+    start() {
+      this.timer?.subscribe((val)=>{
+        this.milliseconds = val
+
+        if(this.milliseconds === 10) {
+    
+          val.next(0)
+          
+
+        }
+      })
+    }
+
+  
+   
+    
+  
+    
 
 }
